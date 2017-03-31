67c426e5a667063445d0681d23612440dec72683
aivankovich <alesandr-ivankovich@ya.ru>
2017-03-31 23:23:28 +0300

    Revert "Такие дела (reverted from commit 7f10106f87057d81fddbf6ab2456fc93579b5744)"
    This reverts commit e83d93dbbc921ffd469ab9a58a0ac852e1036725.

--- views/posts.php
+++ views/posts.php
@@ -3,6 +3,6 @@
         <h3><a href="post.php?id=<?=$a['id']?>"><?=$a['title']?></a></h3>
         <h4><?=$a['name']?></h4>
         <p><?=posts_intro($a['content'])?></p>
-        <em>Электронная почта: <?=$a['mail']?></em>
+        <em>конская залупа: <?=$a['mail']?></em>
     </div>
 <?php endforeach ?>   
